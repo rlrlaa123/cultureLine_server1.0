4e4f7263ed528bbc4a344c58e0a21bcff2958942
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-09-02 17:51:10 +0900

    Answer: store 후 selected 데이터

--- app/Http/Controllers/API/AnswerController.php
+++ app/Http/Controllers/API/AnswerController.php
@@ -71,6 +71,7 @@ class AnswerController extends Controller
 
         $answer->like = 0;
         $answer->author = auth()->user();
+        $answer->selected = false;
 
         return response($answer, 200);
     }
