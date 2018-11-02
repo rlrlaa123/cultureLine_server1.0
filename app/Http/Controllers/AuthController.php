2a597aff931e51daeb17d9471cf39d2cd9582790
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-11-02 19:39:29 +0900

    Auth: pw find

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register', 'idSearch', 'pwSearch']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register', 'idSearch', 'pwReset']]);
     }
 
     /**
